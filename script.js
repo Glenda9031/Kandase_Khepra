27aa462dcfb235459a7555fad35b76d0dedf01a4
Glenda Joyce Reed <glendareed0807@sbcglobal.net>
2023-09-16 11:03:32 -0400

    Changed names in my images folder and updated html, css and javascript for gallery folders

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-"use strict";
-const openMenu = document.querySelector("#open-menu");
-const ul = document.querySelector("nav > ul");
-const closeMenu = document.querySelector("#close-menu");
-console.log(ul);
+const imgEl = document.getElementById('random-image');
+const btn = document.getElementById('btn');
 
-openMenu.addEventListener("click", () => {
-  console.log("I'm being clicked");
-  ul.classList.toggle("show");
-  closeMenu.style.display = "block";
-  openMenu.style.display = "none";
+const srcArray = ['img1.jpg', 'img2.JPG', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.JPG', 'img7.jpg', 'img8.JPG', 'img9.JPG', 'img10.JPG', 'img11.JPG', 'img12.JPG', 'img13.jpg'];
+
+btn.addEventListener('click', () => {
+  window.location.reload();
 });
 
-closeMenu.addEventListener("click", () => {
-  closeMenu.style.display = "none";
-  openMenu.style.display = "block";
-  ul.classList.toggle("show");
-});
+randomImage();
+function randomImage() {
+  const randomIndex = Math.floor(Math.random() * srcArray.length);
+
+  imgEl.src = srcArray[randomIndex];
+
+
+}
+/*const gallery = document.getElementById('gallery');
+const popup = document.getElementById('popup');
+const selectedImage = document.getElementById('selectedImage');
+const imageIndexes = [1,2,3,4,5,6,7,8,9,10];
+const selectedIndex = null;
+
+imageIndexes.forEach(i => {
+  const image = document.createElement('img');
+  image.src = `images/lo-${i}.jpg`;
+  image.alt = `Kandase with lady with bantu braids ${i}`;
+  image.classList.add('galleryImg');
+
+  image.addEventListener('click', () => {
+
+  })
+
+  gallery.appendChild(image);
+});*/
