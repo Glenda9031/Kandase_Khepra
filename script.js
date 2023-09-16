42ae6d1a6d36a141d1b83f0d38980a43da800a8c
Glenda Joyce Reed <glendareed0807@sbcglobal.net>
2023-09-16 17:17:35 -0400

    Update word doc, css and html

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-const imgEl = document.getElementById('random-image');
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = 'img1.jpg';
+images[1] = 'img2.JPG';
+images[2] = 'img3.jpg';
+images[3] = 'img4.jpg';
+images[4] = 'img5.jpg';
+images[5] = 'img6.JPG';
+images[6] = 'img7.jpg';
+images[7] = 'img8.JPG';
+images[8] = 'img9.JPG';
+images[9] = 'img10.JPG';
+images[10] = 'img11.JPG';
+images[11] = 'img12.JPG';
+images[12] = 'img13.jpg';
+
+function changeImg() {
+  document.slide.src = images[i];
+
+  if (i < images.length - 1) {
+    i++
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
+
+
+/*const imgEl = document.getElementById('random-image');
 const btn = document.getElementById('btn');
 
 const srcArray = ['img1.jpg', 'img2.JPG', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.JPG', 'img7.jpg', 'img8.JPG', 'img9.JPG', 'img10.JPG', 'img11.JPG', 'img12.JPG', 'img13.jpg'];
@@ -14,7 +47,7 @@ function randomImage() {
   imgEl.src = srcArray[randomIndex];
 
 
-}
+}*/
 /*const gallery = document.getElementById('gallery');
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
